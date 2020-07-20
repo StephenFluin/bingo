95d2b54b0232dbc7e971f314bc38adbc523e48d1
Stephen Fluin <stephen.fluin@gmail.com>
2020-07-19 17:05:24 -0700

    adding pwa and marketing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Sound } from './sound.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [Sound],
   bootstrap: [AppComponent]
