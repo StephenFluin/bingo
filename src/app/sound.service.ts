5d4c37672d9664beefaa64008e7c9face00ea0f1
Stephen Fluin <stephen.fluin@gmail.com>
2025-05-17 07:47:56 -0500

    feat: bump to angular 20 and remove service worker

--- src/app/sound.service.ts
+++ src/app/sound.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-@Injectable()
+@Injectable({ providedIn: "root" })
 export class Sound {
-    files = [];
-    loadAudioFiles() {
-        for (let i = 1; i <= 75; i++) {
-            let file = new Audio(`/assets/sounds/${i}.mp3`);
-            this.files.push(file);
-        }
+  files = [];
+  loadAudioFiles() {
+    for (let i = 1; i <= 75; i++) {
+      let file = new Audio(`/assets/sounds/${i}.mp3`);
+      this.files.push(file);
     }
-    play(ball: number) {
-        console.log(`Playing sound for ball ${ball}`, this.files[ball - 1]);
-        if (this.files[ball - 1]) {
-            this.files[ball - 1].play()
-                .then(() => { })
-                .catch((error) => {
-                    // Couldn't play the music for this ball, but that's okay
-                });
-        }
+  }
+  play(ball: number) {
+    console.log(`Playing sound for ball ${ball}`, this.files[ball - 1]);
+    if (this.files[ball - 1]) {
+      this.files[ball - 1]
+        .play()
+        .then(() => {})
+        .catch((error) => {
+          // Couldn't play the music for this ball, but that's okay
+        });
     }
+  }
 }
